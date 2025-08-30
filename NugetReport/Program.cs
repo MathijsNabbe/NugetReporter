137133894b6caf6061621dd2e8ae60066ec5ff3b
Mathijs Nabbe <datenenaampje@gmail.com>
2025-08-30 12:09:08 +0200

    Add functionality to parse package versions and project references in `NugetReport`.

--- NugetReport/Program.cs
+++ NugetReport/Program.cs
@@ -1,4 +1,6 @@
-﻿var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
+﻿using System.Xml.Linq;
+
+var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
 
 if (string.IsNullOrWhiteSpace(workspace))
 {
@@ -16,6 +18,9 @@ Console.WriteLine($"Scanning workspace: {workspace}");
 
 var packageFile = GetPackageVersionFile(workspace);
 var projectFiles = GetProjectFiles(workspace);
+
+var packageVersions = LoadPropsVersions(packageFile);
+var references = projectFiles.SelectMany(f => LoadProjectReferences(f)).ToList();
 
 return 0;
 
@@ -41,4 +46,50 @@ string[] GetProjectFiles(string workspace)
     Console.WriteLine($"Detected {projectFiles.Length} project files");
     
     return projectFiles;
+}
+
+Dictionary<string,string> LoadPropsVersions(string propsFile)
+{
+    var xml = XDocument.Load(propsFile);
+    var packages = xml.Descendants("PackageVersion");
+    
+    var packageVersions = new Dictionary<string, string>();
+    foreach (var package in packages)
+    {
+        var packageKey = package.Attribute("Include")?.Value ?? "";
+        var packageVersion = package.Attribute("Version")?.Value ?? "";
+        if (string.IsNullOrWhiteSpace(packageKey) || string.IsNullOrWhiteSpace(packageVersion))
+        {
+            Console.WriteLine("Malformed package reference found in Directory.Packages.props.");
+            continue;
+        }
+        
+        if (packageVersions.ContainsKey(packageKey))
+        {
+            Console.WriteLine($"Duplicate package {package.Attribute("Include")?.Value ?? ""} found");
+            continue;
+        }
+        
+        packageVersions.Add(packageKey, package.Attribute("Version")?.Value ?? "");
+    }
+    
+    return packageVersions;
+}
+
+static IEnumerable<(string Project, string Package)> LoadProjectReferences(string projectFile)
+{
+    var projectName = Path.GetFileName(projectFile);
+    var xml = XDocument.Load(projectFile);
+    var packages = xml.Descendants("PackageReference");
+    foreach (var package in packages)
+    {
+        var packageKey = package.Attribute("Include")?.Value ?? "";
+        if (string.IsNullOrWhiteSpace(packageKey))
+        {
+            Console.WriteLine($"Malformed package reference found in {projectName}.");
+            continue;
+        }
+        
+        yield return (projectName, packageKey);
+    }
 }
